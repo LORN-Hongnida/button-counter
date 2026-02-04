f2abff01325998d73e57d7b5b8ba41ad4f50622e
Numpang <thenumpang@gmail.com>
2026-02-04 16:23:01 +0700

    Fix .gitignore encoding and remove accidentally tracked files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const grt = 'Hello World!';
-console.log(grt);
-function add(a, b) {
-    return a + b;
-}
-console.log(add(1, 2));
-//# sourceMappingURL=index.js.map
