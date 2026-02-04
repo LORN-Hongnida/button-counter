8e7dcada566834c4b2242d976762d871da8e859b
Numpang <thenumpang@gmail.com>
2026-02-04 16:20:44 +0700

    Initialize project with TypeScript setup and generate initial build artifacts.

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const grt = 'Hello World!';
+console.log(grt);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+//# sourceMappingURL=index.js.map
